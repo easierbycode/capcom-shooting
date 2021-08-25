733d6ede4fe1c021c6dd02af9e0ca97392bbb8db
easierbycode <daniel@easierbycode.com>
2021-08-24 18:47:58 -0700

    scale to fit

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,25 @@
-import Phaser from 'phaser'
 
+import Phaser from 'phaser'
 import LoadScene from './scenes/LoadScene'
 import TitleScene from './scenes/TitleScene'
+import { i } from './scenes/LoadScene'
+
 
 const config: Phaser.Types.Core.GameConfig = {
 	type: Phaser.AUTO,
-	width: 800,
-	height: 600,
+	width: i.GAME_WIDTH,
+	height: i.GAME_HEIGHT,
 	physics: {
 		default: 'arcade',
 		arcade: {
 			gravity: { y: 200 }
 		}
 	},
-	scene: [LoadScene, TitleScene]
+	scene: [LoadScene, TitleScene],
+	scale: {
+		autoCenter: Phaser.Scale.Center.CENTER_HORIZONTALLY,
+		mode: Phaser.Scale.ScaleModes.FIT
+	}
 }
 
 const game = window.game = new Phaser.Game(config)
