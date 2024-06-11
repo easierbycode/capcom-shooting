d796cfac01312d7cbdd8b0ffdf80393eb498af7d
EasierByCode <daniel@easierbycode.com>
2024-06-11 09:41:34 -0700

    remove gravity

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,34 @@
-
-import Phaser from 'phaser'
-import AdvScene from './scenes/AdvScene'
-import GameScene from './scenes/GameScene'
-import GameoverScene from './scenes/GameoverScene'
-import LoadScene from './scenes/LoadScene'
-import TitleScene from './scenes/TitleScene'
-import { i } from './scenes/LoadScene'
-
+import Phaser from "phaser";
+import AdvScene from "./scenes/AdvScene";
+import GameScene from "./scenes/GameScene";
+import GameoverScene from "./scenes/GameoverScene";
+import LoadScene from "./scenes/LoadScene";
+import TitleScene from "./scenes/TitleScene";
+import { i } from "./scenes/LoadScene";
 
 const config: Phaser.Types.Core.GameConfig = {
-	type: Phaser.AUTO,
-	width: i.GAME_WIDTH,
-	height: i.GAME_HEIGHT,
-	physics: {
-		default: 'arcade',
-		arcade: {
-			gravity: { y: 200 }
-		}
-	},
-	scene: [LoadScene, TitleScene, AdvScene, GameScene, GameoverScene],
-	scale: {
-		autoCenter: Phaser.Scale.Center.CENTER_HORIZONTALLY,
-		mode: Phaser.Scale.ScaleModes.FIT
-	},
-	render: {
-		pixelArt: true
-	},
-	fps: {
-		target: 30
-	}
-}
+  type: Phaser.AUTO,
+  width: i.GAME_WIDTH,
+  height: i.GAME_HEIGHT,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: new URL(window.location.href).searchParams.get("debug") == "1",
+    },
+  },
+  scene: [LoadScene, TitleScene, AdvScene, GameScene, GameoverScene],
+  scale: {
+    autoCenter: Phaser.Scale.Center.CENTER_HORIZONTALLY,
+    mode: Phaser.Scale.ScaleModes.FIT,
+  },
+  render: {
+    pixelArt: true,
+  },
+  fps: {
+    target: 30,
+  },
+};
 
-const game = window.game = new Phaser.Game(config)
+const game = (window.game = new Phaser.Game(config));
 
-
-export default game
+export default game;
