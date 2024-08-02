0aeaa29dd26c796067b7b5d6b6a2badc6ed63938
EasierByCode <daniel@easierbycode.com>
2024-08-02 12:23:40 -0700

    fix Button events

--- src/scenes/TitleScene.ts
+++ src/scenes/TitleScene.ts
@@ -150,7 +150,7 @@ export class Sprite extends Phaser.GameObjects.Sprite {
   constructor(scene, x, y, texture, frame?) {
     super(scene, x, y, texture, frame);
     this.setOrigin(0);
-    scene.add.existing(this);
+    // scene.add.existing(this);
   }
 }
 
